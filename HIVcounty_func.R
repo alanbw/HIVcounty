9d4b64c4dd63f4a84708b181995f82e7b3e822e2
Ravi Goyal <ravigoyal@Ravis-MacBook-Pro.local>
2023-05-09 15:25:59 -0700

    Included mapping to analysis. Started to create common functions across the counties

---
+++ HIVcounty_func.R
@@ -0,0 +1,24 @@
+
+read_county_data <- function(directory_loc,
+                             county_demo_file_subloc,
+                             county_zip_file_subloc,
+                             county_zip_list 
+                             ) {
+  
+  county_demo.df = read_csv(paste(directory_loc, county_demo_file_subloc, sep="")) %>% 
+    mutate(across(where(is.character), toupper))
+  
+  county_zip.df = read_csv(paste(directory_loc, county_zip_file_subloc, sep="")) %>% 
+    mutate(across(where(is.character), toupper))
+  
+  county_diagnosis.df = left_join(county_demo.df, county_zip.df,
+                                  by = c( "rsd_zip_cd" = "zipcode"))
+  
+  #Filter not diagnosed in county
+  
+
+  county_diagnosis.df  = county_diagnosis.df %>% 
+    filter(rsd_zip_cd %in% county_zip_list)
+  
+  return(county_diagnosis.df)
+}
