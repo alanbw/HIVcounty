10c77c41b9c0a0ed88bc16ee99aa5e4fd6371887
Ravi Goyal <ravigoyal@Ravis-MacBook-Pro.local>
2023-07-19 14:07:13 -0700

    update

--- HIVcounty_func.R
+++ HIVcounty_func.R
@@ -2,7 +2,8 @@
 read_county_data <- function(directory_loc,
                              county_demo_file_subloc,
                              county_zip_file_subloc,
-                             county_zip_list 
+                             county_zip_list,
+                             county_vl_file_subloc
                              ) {
   
   county_demo.df = read_csv(paste(directory_loc, county_demo_file_subloc, sep="")) %>% 
@@ -11,9 +12,14 @@ read_county_data <- function(directory_loc,
   county_zip.df = read_csv(paste(directory_loc, county_zip_file_subloc, sep="")) %>% 
     mutate(across(where(is.character), toupper))
   
+  county_vl.df = read_csv(paste(directory_loc, county_vl_file_subloc, sep="")) %>% 
+    mutate(across(where(is.character), toupper))
+  
   county_diagnosis.df = left_join(county_demo.df, county_zip.df,
                                   by = c( "rsd_zip_cd" = "zipcode"))
   
+  county_diagnosis.df = left_join(county_diagnosis.df, county_vl.df,
+                                  by = c("UCI"))
   #Filter not diagnosed in county
   
 
